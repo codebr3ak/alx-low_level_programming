b65cfa3dd533d6162c70fee783a6ca9292cd1208
codebr3ak <barakaralph@gmail.com>
2022-09-13 00:16:21 +0300

    Final commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - this is where the program start running
+ *Return:returns zero if error and non zero if no errors
+ */
+
+int main(void)
+{
+
+	int c, j;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (c < j)
+			{
+				putchar(c);
+				putchar(j);
+
+				if (c != '8' || (c == '8' && j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
