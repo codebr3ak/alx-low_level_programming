64435d01a543c35effdaf97aa061930be1072421
codebr3ak <barakaralph@gmail.com>
2022-09-15 21:37:29 +0300

    Function for natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024 (excluded)
+ * printf: sends formatted output to screen
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
