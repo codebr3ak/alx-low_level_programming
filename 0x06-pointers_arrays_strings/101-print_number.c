def1653681a7aa61750cba473c023ad4a3c9dab7
codebr3ak <barakaralph@gmail.com>
2022-09-23 20:04:30 +0300

    fix3

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - rotate characters 13 places in the alphabet
- * @s: string
- * Return: string `s` rotated
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ * Return: Nothing
  */
-
-char *rot13(char *s)
+void print_number(int n)
 {
-	int i;
-	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char storel[] = "nopqrstuvwxyzabcdefghijklm";
+	unsigned int num = n;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (n < 0)
 	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
-		{
-			s[i] = (s[i] - 65 > 25) ?
-				storel[s[i] - 97] : storeh[s[i] - 65];
-		}
+		_putchar('-');
+		num = -num;
 	}
-	return (s);
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
