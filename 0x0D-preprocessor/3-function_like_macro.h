7a454971da737d76ed9e58fba7d69a28d51db098
codebr3ak <barakaralph@gmail.com>
2022-10-13 08:27:16 +0300

    code fix for task 4

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
-
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#ifndef SUM_H
+#define SUM_H
+#define SUM(x, y) (x + y)
+#endif
