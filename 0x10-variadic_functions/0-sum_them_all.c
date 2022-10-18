c97deff596f821312210b605caf7298f53c396de
codebr3ak <barakaralph@gmail.com>
2022-10-18 14:05:14 +0300

    At this point I am done with fixing this code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,28 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
-*sum_them_all - sums all its parameters
-*@n: first parameter
-*
-*Return: 0 if n is null
-* or sum of parameters in other case
-*/
-
-int sum_them_all(const unsigned int n, ...)
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list list;
 
-va_list li;
-int sum = 0;
-unsigned int i;
+	va_start(list, n);
 
-va_start(li, n);
+	for (i = 0; i < n; i++)
+	{
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
+	}
 
-if (n != 0)
-{
-for (i = 0; i < n; i++)
-sum += va_arg(li, int);
-}
+	va_end(list);
 
-va_end(li);
-return (sum);
-
+	printf("\n");
 }
