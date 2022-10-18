1e87a45da88912a106b36c382a505057c7362d36
codebr3ak <barakaralph@gmail.com>
2022-10-18 14:21:52 +0300

    Hopefully this is my last fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,22 +3,23 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-
-typedef struct printer
-{
-char *symbol;
-void (*print)(va_list arg);
-}
-printer_t;
-
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
 void print_all(const char * const format, ...);
+
+
+/**
+ * struct fm - structure
+ * @fm: Pointer
+ * @p: pointer
+ */
+
+typedef struct fm
+{
+	char *fm;
+	void (*p)();
+} format_type;
+
 
 #endif
