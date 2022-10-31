e7df9b35ba652ba3a9e926891154e60376870024
codebr3ak <barakaralph@gmail.com>
2022-10-31 21:35:43 +0300

    task four

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
-* clear_bit - sets the value of a bit to 0 at a given index
-* @n: pointer of an unsigned long int
-* @index: index of the bit.
-* Return: 1 if it worked, -1 if it didn't
+*set_bit - sets a bit at position to 1
+*@n: number to be used
+*@index: index to be set
+*
+*Return: 1 on success, -1 on failure
 */
-int clear_bit(unsigned long int *n, unsigned int index)
-{
-	unsigned int m;
 
-	if (index > 63)
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
-	m = 1 << index;
-	if (*n & m)
-		*n ^= m;
+
+	*n |= (1 << index);
 	return (1);
 }
